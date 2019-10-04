35b50d425209773c9dd21f125b1bd3f8c3daa46b
Andre Miranda <andreldm@xfce.org>
2019-10-04 00:45:46 -0300

    Allow compilation with panel 4.15

--- panel-plugin/systemload.c
+++ panel-plugin/systemload.c
@@ -38,7 +38,7 @@
 
 #include <libxfce4util/libxfce4util.h>
 #include <libxfce4ui/libxfce4ui.h>
-#include <libxfce4panel/xfce-panel-plugin.h>
+#include <libxfce4panel/libxfce4panel.h>
 
 #ifdef HAVE_UPOWER_GLIB
 #include <upower.h>
