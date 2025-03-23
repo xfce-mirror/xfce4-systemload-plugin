44778c3692d8733a39276092a968e0440491dd18
Bobby Rong <rjl931189261@126.com>
2025-03-23 17:28:37 +0800

    Add meson build

--- panel-plugin/settings.cc
+++ panel-plugin/settings.cc
@@ -32,10 +32,7 @@
 #include <config.h>
 #endif
 
-#ifdef HAVE_STRING_H
 #include <string.h>
-#endif
-
 #include <libxfce4panel/libxfce4panel.h>
 #include <libxfce4util/libxfce4util.h>
 #include <libxfce4ui/libxfce4ui.h>
